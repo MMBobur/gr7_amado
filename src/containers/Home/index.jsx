c5e569c69241a84f9332b6c334d856f92419071d
Rustamjon20022 <rajabovrustamjon811@gmail.com>
2022-06-17 17:15:26 +0500

    dfdf

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
 import Content from '../../components/Sidebar';
+import Homes from './Homes';
 
 export default () => (
   <div>
     {/* <Sidebar /> */}
     {/* <Content /> */}
+    <Homes />
   </div>
 );
